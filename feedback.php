e15628eec8d4cedf9339f2b355e2b3531310768c
pasindu-uthum <pasinduuthum31@gmail.com>
2021-09-05 22:55:40 +0530

    Changed the feedback.php

--- feedback.php
+++ feedback.php
@@ -1,23 +1,14 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['mail'];
+	$message = $_POST['message'];
 
-$email_from = 'pasinduuthum31@gmail.com';
+	$mailTo = 'pasinduuthum31@gmail.com';
+	$headers = "From: " . $mailFrom;
+	$txt = "You have recived an email from " . $name . ".\n\n" . $message;
 
-$email_subject = "FeedBack from the TODO Application";
-
-$email_body = "User Name: $name.\n".
-		"User Email: $visitor_email.\n".
-			"User Message: $message.\n";
-
-$to = "pasinduuthum31@gmail.com";
-
-$headers = "From: $email_from \r\n";
-
-$headers = "Replay-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: index.html");
-?>
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.html?mailsend");
+}
